22dedfbba383ee2207af1e191c85bc99a59782c4
Ducko <duckodas@gmail.com>
2025-06-12 13:01:47 +0200

    Update logger.ts

--- packages/bot/src/logger.ts
+++ packages/bot/src/logger.ts
@@ -3,25 +3,26 @@ import { createLogger, transports, format } from 'winston';
 import { generateErrorID } from './components/exports.js';
 
 const consoleFormat = format.printf(({ level, message, timestamp }) => {
-	return `[${timestamp}] [${level}] | ${JSON.stringify(message)}`;
+    return `[${timestamp}] [${level}] | ${JSON.stringify(message)}`;
 });
 
 const transport_logs: any = [
-	new transports.Console({
-		format: format.combine(
-			format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
-			format.colorize(),
-			format.splat(),
-			consoleFormat,
-		),
-	}),
+    new transports.Console({
+        format: format.combine(
+            format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
+            format.colorize(),
+            format.splat(),
+            consoleFormat,
+        ),
+    }),
 ];
 
 const logger = createLogger({
-	level: process.env.NODE_ENV === 'production' ? 'info' : 'debug',
-	transports: transport_logs,
-	exceptionHandlers: [new transports.File({ filename: 'logs/bot/exceptions.log' })],
-	rejectionHandlers: [new transports.File({ filename: 'logs/bot/rejections.log' })],
+    level: process.env.NODE_ENV === 'production' ? 'info' : 'debug',
+    transports: transport_logs,
+    exitOnError: false,
+    exceptionHandlers: [new transports.File({ filename: 'logs/bot/exceptions.log' })],
+    rejectionHandlers: [new transports.File({ filename: 'logs/bot/rejections.log' })],
 });
 
 /**
@@ -32,38 +33,38 @@ const logger = createLogger({
  */
 
 export async function logException(interaction: CommandInteraction | null, e: any): Promise<string> {
-	const errorId = generateErrorID();
-	e = e.message ?? e;
+    const errorId = generateErrorID();
+    e = e.message ?? e;
 
-	if (interaction) {
-		const args = interaction.options.data.map((x) => {
-			return { name: x.name, value: x.value };
-		});
+    if (interaction) {
+        const args = interaction.options.data.map((x) => {
+            return { name: x.name, value: x.value };
+        });
 
-		logger.error({ labels: { command: interaction.commandName, errorId }, message: { e, args } });
+        logger.error({ labels: { command: interaction.commandName, errorId }, message: { e, args } });
 
-		try {
-			interaction.editReply({
-				embeds: [
-					new EmbedBuilder()
-						.setColor('Red')
-						.setDescription(
-							[
-								`\`🔴\` An unexpected error has occurred`,
-								`Error ID: \`${errorId}\``,
-								`Please report this in the [Online Safety Support](<https://discord.com/invite/P3bfEux5cv>)`,
-							].join('\n'),
-						),
-				],
-			});
-		} catch (err) {
-			console.error(err);
-		}
-	} else {
-		logger.error({ labels: { errorId }, message: { e } });
-	}
+        try {
+            interaction.editReply({
+                embeds: [
+                    new EmbedBuilder()
+                        .setColor('Red')
+                        .setDescription(
+                            [
+                                `\`🔴\` An unexpected error has occurred`,
+                                `Error ID: \`${errorId}\``,
+                                `Please report this in the [Online Safety Support](<https://discord.com/invite/P3bfEux5cv>)`,
+                            ].join('\n'),
+                        ),
+                ],
+            });
+        } catch (err) {
+            console.error(err);
+        }
+    } else {
+        logger.error({ labels: { errorId }, message: { e } });
+    }
 
-	return errorId;
+    return errorId;
 }
 
 export default logger;
